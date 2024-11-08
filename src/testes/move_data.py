d78a1d9ce7e24b25a50e3e2c893eac83cbb35412
Davi Martignoni Barros <davimartignonibarros@gmail.com>
2024-11-08 09:43:46 -0300

    creating move quality feature, in progress

--- src/testes/move_data.py
+++ src/testes/move_data.py
@@ -1,12 +1,13 @@
 import chess
 
 class MoveData():
-    def __init__(self, move: chess.Move, board: chess.Board, eval: dict) -> None:
+    def __init__(self, move: chess.Move, board: chess.Board, eval: dict, previous_eval: dict) -> None:
         self.move = move
         self.board = board
         self.eval = eval
         self.player = board.turn
         self._count_material()
+        self._evaluate_quality(previous_eval)
     
     def piece_moved(self):
         return self.board.piece_at(self.to_square).symbol()
@@ -32,5 +33,35 @@ class MoveData():
             else:
                 self.black_material += value
 
+    # def _evaluate_quality(self, previous_eval: dict):
+
+    #     # From centipawn to centipawn
+    #     if previous_eval['type'] == 'cp' and self.eval['type'] == 'cp':
+
+    #         # Assuming the player will never make the evaluation increase, we calculate how much it decreased in absolute value
+    #         decrease = abs(self.eval['value'] - previous_eval['value'])
+            
+    #         if decrease == 0:
+    #             self.quality = 'Best'
+    #         elif decrease < 50:
+    #             self.quality = 'Excellent'
+    #         elif decrease < 100: 
+    #             self.quality = 'Good'
+    #         elif decrease < 200:
+    #             self.quality = 'Inaccuracy'
+    #         elif decrease < 300:
+    #             self.quality = 'Mistake'
+    #         else:
+    #             self.quality = 'Blunder'
+    #     # Mate sequence changed to centipawn, a mate was missed
+    #     elif previous_eval['type'] == 'mate' and self.eval['type'] == 'cp':
+    #         self.quality = 'Missed Mate'
         
+    #     # Centipawn changed to mate
+    #     # Either one player blundered a mate, or stockfish had a high centipawn eval but hadn't seen the mate sequence
+    #     elif previous_eval['type'] == 'cp' and self.eval['type'] == 'mate':
+    #         if (self.player == chess.WHITE and )
+    #         self.quality = 'Blunder'
+
+    #     else: # both evals are of type mate
 
